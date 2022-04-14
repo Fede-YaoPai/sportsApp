4dc986b9cf2b8737f0d298fb5bb10c5f3f48747f
Fede-YaoPai <federico.lecca@hotmail.com>
2022-04-14 22:44:33 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/layout/header/header.component';
+import { FooterComponent } from './core/layout/footer/footer.component';
+import { HomeComponent } from './features/home/home.component';
+import { AboutComponent } from './features/about/about.component';
+import { ProductsComponent } from './features/products/products.component';
+import { ApplyButtonComponent } from './shared/components/apply-button/apply-button.component';
+import { LoginFormComponent } from './shared/components/login-form/login-form.component';
+import { CancelButtonComponent } from './shared/components/cancel-button/cancel-button.component';
+import { MainNavbarComponent } from './shared/components/main-navbar/main-navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ProductsComponent,
+    ApplyButtonComponent,
+    LoginFormComponent,
+    CancelButtonComponent,
+    MainNavbarComponent
   ],
   imports: [
     BrowserModule,
