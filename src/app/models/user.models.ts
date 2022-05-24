bfc7ed2bef418f31183cdd4cbc7680c95460b5f5
Federico Lecca <federico.lecca@engagegroup.it>
2022-05-24 18:32:56 +0200

    asdasd

--- src/app/models/user.models.ts
+++ src/app/models/user.models.ts
@@ -1,5 +1,19 @@
+// export interface User {
+//   username: string,
+//   email: string,
+//   password: string
+// }
+
 export interface User {
   username: string,
   email: string,
-  password: string
+  password: string,
+  address: UserAddress
+}
+
+export interface UserAddress {
+  country: string,
+  city: string,
+  street: string,
+  number: number
 }
--- src/app/shared/components/reactive-form/reactive-form.component.ts
+++ src/app/shared/components/reactive-form/reactive-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { UserService } from 'src/app/core/services/user.service';
 import { User } from 'src/app/models/user.models';
 
@@ -12,39 +12,57 @@ export class ReactiveFormComponent implements OnInit {
 
   public registrationForm!: FormGroup;
 
-  constructor(private userService: UserService) { }
+  constructor(
+    private userService: UserService,
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
     this.initRegistrationForm();
   }
 
+  // private initRegistrationForm(): void {
+  //   this.registrationForm = new FormGroup({
+  //     username: new FormControl('', [Validators.required]),
+  //     email: new FormControl('', [Validators.required]),
+  //     password: new FormControl('', [Validators.required])
+  //   });
+  // }
+
   private initRegistrationForm(): void {
-    this.registrationForm = new FormGroup({
-      username: new FormControl('', [Validators.required]),
-      email: new FormControl('', [Validators.required]),
-      password: new FormControl('', [Validators.required])
+    this.registrationForm = this.fb.group({
+      username: ['', {validators: [Validators.required], asyncValidators: [], updateOn: 'blur'}],
+      email: ['', {validators: [Validators.required], asyncValidators: [], updateOn: 'blur'}],
+      password: ['', {validators: [Validators.required], asyncValidators: [], updateOn: 'blur'}],
+
+      address: this.fb.group({
+        country: [],
+        city: [],
+        street: [],
+        number: []
+      })
     });
   }
 
   public onSubmit(form: FormGroup) {
-    let newUser: User = this.getUserInitialState();
+    // let newUser: User = this.getUserInitialState();
 
-    for (let key in newUser) {
-      newUser[key as keyof User] = form.controls[key].value;
-    }
+    // for (let key in newUser) {
+    //   newUser[key as keyof User] = form.controls[key].value;
+    // }
 
-    this.userService.addNewUser(newUser)
-      .subscribe((u: User) => console.log('added user: ', u));
+    // this.userService.addNewUser(newUser)
+    //   .subscribe((u: User) => console.log('added user: ', u));
   }
 
-  private getUserInitialState(): User {
-    const newUser: User = {
-      username: '',
-      email: '',
-      password: ''
-    }
+  // private getUserInitialState(): User {
+  //   const newUser: User = {
+  //     username: '',
+  //     email: '',
+  //     password: ''
+  //   }
 
-    return newUser;
-  }
+  //   return newUser;
+  // }
 
 }
